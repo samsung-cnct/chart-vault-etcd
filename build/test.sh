2d44a4c076273645f812b9e6af26d2d51dc9c827
Jim Conner <snafu.x@gmail.com>
2017-11-20 10:35:50 -0800

    fixing use of double-brackets in script. I guess the container in gitlab does not allow them. Also fixed shebang.

--- build/test.sh
+++ build/test.sh
@@ -1,12 +1,12 @@
-#! /bin/sh
+#!/bin/sh
 
-[[ -z "$chart_name"   ]] && \
+[ -z "$chart_name" ] && \
   {
-    echo >& "Var '$chart_name' is empty. Cannot continue."
+    echo >&2 "Var '$chart_name' is empty. Cannot continue."
     exit 2
   }
 
-[[ ! -d "${chart_name}" ]] && \
+[ ! -d "${chart_name}" ] && \
   {
     echo >&2 "Directory for chart '$chart_name' does not exist."
     exit 4
