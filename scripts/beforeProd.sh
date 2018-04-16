ee33cd41c9f726eaf5926b2369b63334ceaf4e47
Marat G <maratoid@gmail.com>
2018-04-16 13:14:52 -0700

    prod script needs tools

--- scripts/beforeProd.sh
+++ scripts/beforeProd.sh
@@ -1,11 +1,30 @@
 #! /usr/local/bin/bash -ex
+# setup kubectl
+echo "Setting up kubectl"
+wget https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
 # don't regenerate secrets for prod if they are already present
 if kubectl get secret etcd-client-tls -n prod; then
   exit 0;
 fi
 
+# setup golang
+echo "Setting up golang"
+wget https://redirector.gvt1.com/edgedl/go/go1.9.2.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.9.2.linux-amd64.tar.gz
+mkdir -p /go
 export PATH=$PATH:/usr/local/go/bin:/go/bin
 export GOPATH=/go
+mkdir -p /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
+apk add --no-cache --virtual .build-deps gcc build-base libtool sqlite-dev git curl
+
+# setup cloudflare ssl
+echo "Setting up cloudflare SSL tools"
+go get -u github.com/cloudflare/cfssl/cmd/cfssl
+go get -u github.com/cloudflare/cfssl/cmd/cfssljson
+
 
 # setup tls for etcd
 export GEN_CLUSTER_SIZE=3
